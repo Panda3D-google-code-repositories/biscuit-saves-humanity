ec21e0f28de639de2951334c34ee9a6522ca00a4
justin.c.sweitzer <justin.c.sweitzer@gmail.com>
2013-04-06 04:01:17 +0000

    Added header to explain what we want to do with this file

--- master/main.py
+++ master/main.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python
+#######################################################
+# main.py - Handles creation of all objects and game logic
+# Authors : Justin Sweitzer, Ben Sweitzer
+# Started development 04/04/2013
+# Version 0.1
+#
+#######################################################
+
 
 import sys
 
